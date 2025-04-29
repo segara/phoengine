e701c7bf36676ed290020a647d17021eea0f3564
segara <segara@com2us.com>
2025-04-29 17:23:34 +0900

    cmake fix

--- ProjDay2/Dependency.cmake
+++ ProjDay2/Dependency.cmake
@@ -7,23 +7,23 @@ set(DEP_INCLUDE_DIR ${DEP_INSTALL_DIR}/include)
 set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
 
 IF (WIN32)
-# glfw
-ExternalProject_Add(
-    dep_glfw
-    GIT_REPOSITORY "https://github.com/glfw/glfw.git"
-    GIT_TAG "3.3.2"
-    GIT_SHALLOW 1
-    UPDATE_COMMAND ""
-    PATCH_COMMAND ""
-    CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
-        -DGLFW_BUILD_EXAMPLES=OFF
-        -DGLFW_BUILD_TESTS=OFF
-        -DGLFW_BUILD_DOCS=OFF
-    TEST_COMMAND ""
-    )
-set(DEP_LIST ${DEP_LIST} dep_glfw)
-set(DEP_LIBS ${DEP_LIBS} glfw3)
+# # glfw
+# ExternalProject_Add(
+#     dep_glfw
+#     GIT_REPOSITORY "https://github.com/glfw/glfw.git"
+#     GIT_TAG "3.3.2"
+#     GIT_SHALLOW 1
+#     UPDATE_COMMAND ""
+#     PATCH_COMMAND ""
+#     CMAKE_ARGS
+#         -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+#         -DGLFW_BUILD_EXAMPLES=OFF
+#         -DGLFW_BUILD_TESTS=OFF
+#         -DGLFW_BUILD_DOCS=OFF
+#     TEST_COMMAND ""
+#     )
+# set(DEP_LIST ${DEP_LIST} dep_glfw)
+# set(DEP_LIBS ${DEP_LIBS} glfw3)
 ENDIF()
 
 
